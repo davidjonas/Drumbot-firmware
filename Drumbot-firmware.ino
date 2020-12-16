3389b87bdb925619689b423af537eac5bce1e4b1
David Jonas <davidjonasdesign@gmail.com>
2020-12-16 14:49:49 +0100

    Added velocity but still not working well

--- Drumbot-firmware.ino
+++ Drumbot-firmware.ino
@@ -4,9 +4,10 @@
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 int pins [] =     {13, 12, 11, 10, 8,  7};
-int notes [] =    {60, 62, 64, 65, 67, 69};
+int notes [] =    {60, 61, 62, 63, 64, 65};
 int onCycles [] = {0,  0,  0,  0,  0,  0};
-int securityLimit = 90000;
+int maxDuration = 2000;
+int minDuration = 1000;
 int availableNotes = 0;
 
 void reset()
@@ -22,19 +23,23 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
     int pin;
 
-    for(int i=0; i<availableNotes; i++)
-    {
-      if(notes[i] == pitch)
+    if(velocity > 0){
+      for(int i=0; i<availableNotes; i++)
       {
-        pin = pins[i];
-        onCycles[i] = 1;
-        break;
+        if(notes[i] == pitch)
+        {
+          pin = pins[i];
+          onCycles[i] = map((int)velocity, 1, 127, minDuration, maxDuration);
+          break;
+        }
       }
+  
+      digitalWrite(pin,HIGH);
     }
-
-    digitalWrite(pin,HIGH);
 }
 
+
+//Never called right now
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
   int pin;
@@ -63,21 +68,22 @@ void setup()
   for(int i=0; i<availableNotes; i++)
   {
     pinMode(pins[i],OUTPUT);
+    digitalWrite(pins[i],LOW);
   }
 
   reset();
 }
 
-void securityCheck()
+void delayCheck()
 {
     for(int i=0; i<availableNotes; i++)
     {
-      if(onCycles[i] > 0)
+      if(onCycles[i] >= 0)
       {
-        onCycles[i] = onCycles[i] + 1;
+        onCycles[i]--;
       }
 
-      if(onCycles[i] > securityLimit)
+      if(onCycles[i] <= 0)
       {
         digitalWrite(pins[i],LOW);
         onCycles[i] = 0;
@@ -89,5 +95,5 @@ void securityCheck()
 void loop()
 {
     MIDI.read();
-    securityCheck();
+    delayCheck();
 }
